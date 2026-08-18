b27ba06d09dc02ae4323d1dfc4b2abda090c76a9
webbchrisuk-max <webbchris.uk@icloud.com>
2026-08-18 12:59:19 +0100

    Publish Aurora 2.8.18.1 club command release

--- aurora-release.js
+++ aurora-release.js
@@ -2,12 +2,12 @@
 'use strict';
 w.AuroraRelease=Object.freeze({
   product:'Aurora 2.0',
-  release:'Stable Core v1',
-  build:'2026.08.15.1',
+  release:'Club Command v1',
+  build:'2026.08.18.1',
   schemaVersion:11,
   syncManagerVersion:1,
   platformVersion:1,
-  releasedAt:'2026-08-15T12:00:00Z',
+  releasedAt:'2026-08-18T11:53:00Z',
   channel:'stable-candidate'
 });
 })(window);
